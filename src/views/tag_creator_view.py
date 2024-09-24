eca51325ebbb9d702368de9de8f4317cf094a298
GabrielF <gabrielFMendonca@gmail.com>
2024-09-24 20:03:35 -0300

    Adding controllers

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,16 +1,19 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
 
 class TagCreatorView:
 
     # responsability for interacting with HTTP
 
     def validade_and_create(self, http_request: HttpRequest):
-        #body = http_request.body
-        #product_code = body["product_code"]
+        tag_creator_controller = TagCreatorController()
+
+        body = http_request.body
+        product_code = body["product_code"]
 
         # logica de regra de negocio
-        print('estou na minha view')
-        print(http_request)
+        formatted_response = tag_creator_controller.create(product_code)
+
         # retorno http
-        return HttpResponse(status_code=200, body={"hello":"world"})
+        return HttpResponse(status_code=200, body=formatted_response)
